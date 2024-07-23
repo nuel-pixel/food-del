61b67b5c0852fefbfa9f1df4a0ef3e60ede0ea82
nuel-pixel <nrohi506@gmail.com>
2024-07-23 13:18:28 +0000

    cart bug

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,37 +1,37 @@
-import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+// import { createContext, useEffect, useState } from "react";
+// import { food_list } from "../assets/assets";
 
-export const StoreContext = createContext(null);
+// export const StoreContext = createContext(null);
 
-const StoreContextProvider = (props) => {
-  const [cartItems, setCartItems] = useState({});
+// const StoreContextProvider = (props) => {
+//   const [cartItems, setCartItems] = useState({});
 
-  const addToCart = (itemId) => {
-    if (!cartItems[itemId]) {
-      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
-    } else {
-      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-    }
-  };
-  const removeFromCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-  };
+//   const addToCart = (itemId) => {
+//     if (!cartItems[itemId]) {
+//       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+//     } else {
+//       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+//     }
+//   };
+//   const removeFromCart = (itemId) => {
+//     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+//   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+//   useEffect(() => {
+//     console.log(cartItems);
+//   }, [cartItems]);
 
-  const contectValue = {
-    food_list,
-    cartItems,
-    addToCart,
-    removeFromCart,
-  };
-  return (
-    <StoreContext.Provider value={contectValue}>
-      {props.children}
-    </StoreContext.Provider>
-  );
-};
+//   const contectValue = {
+//     food_list,
+//     cartItems,
+//     addToCart,
+//     removeFromCart,
+//   };
+//   return (
+//     <StoreContext.Provider value={contectValue}>
+//       {props.children}
+//     </StoreContext.Provider>
+//   );
+// };
 
-export default StoreContextProvider;
+// export default StoreContextProvider;
